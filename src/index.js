e361c038ce12f66cfe57053a65dfde05405309a0
Tran Khoi (IT-SWE-CC1-JV2) <khoi.tran@sbb.ch>
2019-02-06 22:08:54 +0100

    ready

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import * as d3 from 'd3'
+import * as topojson from 'topojson'
 
 import 'reset-css'
 import './style/main.css'
 
 const data = require('./assets/ga_htax.csv')
+const switzerlandTopoJson = require('./assets/ch-plz.json')
 
 async function main() {
     // converts CSV string to array
-    const ga_htax = d3.csvParse(data)
+    const ga_htax = d3.csvParse(data, row => {
+        row.GA = parseFloat(row.GA)
+        row.PLZ = +row.PLZ
+        row.Jahr = +row.Jahr
+        row.HTA = parseFloat(row.HTA)
+        return row
+    })
+    
+    const switzerlandGeoJson = topojson.feature(switzerlandTopoJson, switzerlandTopoJson.objects.plz)
+    const ga_htax_2017 = ga_htax.filter(d => d.Jahr === 2017)
 
-    // do stuff with data
+    console.log(switzerlandGeoJson)
+    console.log(ga_htax_2017)
 }
 
 main()
